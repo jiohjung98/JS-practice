2bf07fcce4eeeafb03ea3117ce238f6db259ae92
jiohjung98 <ckaquz98@naver.com>
2023-08-05 17:29:08 +0900

    ch07.모듈- 동적으로 모듈 가져오기, 가져온 모듈 바로 내보내기

---
+++ ch07. 모듈/3. 동적으로 모듈 가져오기, 가져온 모듈 바로 내보내기/main.js
@@ -0,0 +1,15 @@
+// import * as abc from './module.js'
+// console.log(abc)
+
+
+// setTimeout(async () => {
+//     // import('./module.js').then((abc) => {
+//     //     console.log(abc)
+//     // })
+//     const abc = await import('./module.js')
+//     console.log(abc)
+// }, 1000)
+import { a, b } from './util.js'
+
+console.log(a())
+console.log(b())
---
+++ ch07. 모듈/3. 동적으로 모듈 가져오기, 가져온 모듈 바로 내보내기/module.js
@@ -0,0 +1,11 @@
+// 기본 내보내기(default 키워드는 딱 한 번만 사용 가능)
+export default 123
+
+
+// 이름 내보내기(여러 개 가능!)
+export const str = 'ABC'
+export const arr = []
+export const hello = () => {}
+
+
+// 기본 내보내기는 내가 이름을 정할 수 있고, 이름 내보내기는 이름에 맞게 내보내야 함!
