f3fd9660a3b411ded96a17aad11033b1c2e1de06
jiohjung98 <ckaquz98@naver.com>
2023-08-05 17:21:36 +0900

    ch07.모듈- 가져오기와 내보내기 패턴

---
+++ ch07. 모듈/2. 가져오기와 내보내기 패턴/main.js
@@ -0,0 +1,9 @@
+ import number, {str, arr, hello as h} from "./module.js"
+ 
+ // import 뒤에 *(와일드카드) 넣으면 모든 데이터 가져올 수 있음
+
+console.log(number)
+console.log(str)
+console.log(arr)
+// console.log(hello) -> 에러 발생
+console.log(h)
