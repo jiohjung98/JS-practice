d646dbaaa85212c49d10fe86e5efa1d53737705f
jiohjung98 <ckaquz98@naver.com>
2023-08-24 14:25:19 +0900

    ch11. Location

---
+++ ch11. 기타 Web APIs/3. Location/main.js
@@ -0,0 +1,17 @@
+// Location
+
+// 현재 페이지 정보를 반환하거나 제어
+
+// .href: 전체 URL 주소
+// .protocol: 프로토콜
+// .hostname: 도메인 이름
+// .pathname: 도메인 이후 경로
+// .host: 포트 번호를 포함한 도메인 이름
+// .port: 포트 번호
+// .hash: 해시 정보(페이지 ID)
+
+// .assign(주소): 해당 주소로 페이지 이동
+// .replace(주소): 해당 주소로 페이지 이동, 현재 페이지 히스토리 제거
+// .reload(주소): 페이지 새로고침, true 인수는 강력 새로고침
+
+console.log(location)
